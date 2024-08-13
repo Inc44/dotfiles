6ebf4ed4b4dc4025d61693ac549c96265c9a3bbe
Inc <121856799+Inc44@users.noreply.github.com>
2024-08-13 14:27:32 +0200

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,5 @@
+{
+	"${GREEK}": "alpha|beta|gamma|Gamma|delta|Delta|epsilon|varepsilon|zeta|eta|theta|Theta|iota|kappa|lambda|Lambda|mu|nu|omicron|xi|Xi|pi|Pi|rho|sigma|Sigma|tau|upsilon|Upsilon|varphi|phi|Phi|chi|psi|Psi|omega|Omega",
+	"${SYMBOL}": "hbar|ell|nabla|infty|leftrightarrow|mapsto|setminus|mid|bigcap|bigcup|cap|cup|land|lor|subseteq|subset|implies|impliedby|iff|exists|forall|equiv|square|neq|geq|leq|gg|ll|sim|simeq|approx|propto|oplus|otimes|times|star|perp|det|exp|ln|log|partial|angle|cong|div|max|min|neg|wedge",
+	"${SHORT_SYMBOL}": "to|pm|mp"
+}
